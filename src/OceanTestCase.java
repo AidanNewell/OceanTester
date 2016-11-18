e973410cff66a96c1610a828254a74fa5614b3da
Allison Wei <weiallison1999@gmail.com>
2016-11-18 12:36:11 -0500

    i started lool u do the rest <3

---
+++ src/OceanTestCase.java
@@ -0,0 +1,14 @@
+
+public class OceanTestCase extends AssignmentTestCase{
+	private Boat[] boats;
+	private Position[] positions;
+	public OceanTestCase(Boat[] boats, Position[] positions){
+		this.boats = boats;
+		this.positions = positions;
+	}
+	protected boolean runTestCase() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
