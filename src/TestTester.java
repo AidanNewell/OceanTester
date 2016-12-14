b861d3c9443e6f275fb8dec2c94085f6902c3f9d
Allison Wei <weiallison1999@gmail.com>
2016-12-14 13:16:30 -0500

    stop deleting my ocean

---
+++ src/TestTester.java
@@ -0,0 +1,13 @@
+
+public class TestTester {
+	public static void main (String args[]){
+		OceanTester test = new OceanTester();
+		AssignmentTestCase[] testsss = test.getCases();
+		for(int x=0; x<testsss.length;x++)
+		{
+			try{
+			testsss[x].runTestCase();
+			}catch(Exception e){System.out.println(x);e.printStackTrace();}
+		}
+	}
+}
